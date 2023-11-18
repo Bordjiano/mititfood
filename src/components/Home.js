ed460e942f80c270fe4df01c4b073f4f1173e98f
Bordjiano <vanya.misyura@gmail.com>
2023-11-18 13:02:48 +0200

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = ({ recipes, loading, error }) => {
       {!loading && !error && recipes.length === 0 ? (
         <div>
           <p className="text-2xl lg:text-4xl font-semibold">
-            Начніть пошук!
+            Поле пошуку пусте!
           </p>
         </div>
       ) : null}
